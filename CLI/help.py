fb5a818a2ae628cdb91d1d1a17be4b6e261a9509
Watoc Masc <oushen.m4@gmail.com>
2024-03-20 00:11:32 +0200

    Update section "help", initialization of code words, refactoring

--- CLI/help.py
+++ CLI/help.py
@@ -1,23 +1,21 @@
+from termcolor import colored as cd
 helper = \
-'''
-The data consists of blocks, the blocks 
-have a name and content.
-
-(commands)
-    add [title] [additionally, .., ..] 
+f'''
+{cd("The data consists of blocks, the blocks"
+"have a name and content.", "green")}
+{cd('Commands', 'red')}
+    {cd('add', 'blue')} {cd('<title>', 'magenta')}  {cd('[additionally, .., ..]', 'magenta')} 
 Adding a block with content.
 [title] - the name of the block, serves 
     as the key to open the block data.
 [additionally, .., ..] - the content of 
     the block can contain any data.
-
-    del [title] 
-
-    show [title]
-
-    edit [title]
-
-    show blocks (maybe)
-
-exit -
+    {cd('delete', 'blue')} 
+Deletes a data block by block name.   
+    {cd('show', 'blue')}
+Shows block data by name.   
+    {cd('edit', 'blue')}
+Edits the block data, by its name. 
+    {cd('exit', 'blue')} 
+Terminates the program.
 '''
--- CLI/main.py
+++ CLI/main.py
@@ -1,4 +1,5 @@
-import os, json, time, pyperclip, help
+import os, json, time, help
+from termcolor import colored as cd
 
 
 fileData = 'data.json'
@@ -12,44 +13,54 @@ def showData(dataUser):
         data = json.load(file)
 
     if data['datas'] == {}:
-        print("The data blocks do not exist.\nTip: Start by adding blocks")
+        print(cd("The data blocks do not exist.", 'red'))
+        print(cd("Tip: Start by adding blocks", 'green'))
         start()
     with open(fileData, 'r') as file:
         data = json.load(file)
         
     order = 1
     listBlocks = data['datas'].keys()
-    print(f'————————————————— Titles {20*'—'}')
+    print(f'{20*"—"} Titles {20*"—"}')
     for key in listBlocks:
         print(f'{order}.', key)
         order += 1
-    print(f'{45*'—'}')
+    print(f'{48*"—"}')
     selectBlock = input('Write the name or number to view: ').strip()
     order = 0
+    status = None
     try:
         for key in listBlocks:
             if int(selectBlock)-1 == order:
-                print(f'—————————————————{20*'—'}')
-                print(f'{key}:', ''.join(data['datas'][key]))
-                print(f'—————————————————{20*'—'}')
-                start()
+                status = f"{key}: {''.join(data['datas'][key])}"
+                break
             order += 1
+        if status:
+            print(f'{48*"—"}')
+            print(cd(status, 'green'))
+            print(f'{48*"—"}')
+        else:
+            print(cd('Error!', 'red'))
+        start()     
     except ValueError:
         try:
             if data['datas'][selectBlock]:
-                print(f'—————————————————{20*'—'}')
-                print(f'{selectBlock}:',''.join(data['datas'][selectBlock]))
-                print(f'—————————————————{20*'—'}')
+                print(f'{48*"—"}')
+                print(cd(f"{selectBlock}: {''.join(data['datas'][selectBlock])}", 'green'))
+                print(f'{48*"—"}')
+            else:
+                print(cd('Error!', 'red'))
+            start()
         except KeyError:        
             start()
 
         
-def addData(dataUser):
+def addBlock(dataUser):
     with open(fileData, 'r') as file:
         base = json.load(file)
 
     if dataUser[0] in base['datas']:   
-        print('The block name already exists!')
+        print(cd('The block name already exists!', 'red'))
         start()
     elif len(dataUser) > 1:
         new_block = {f'{dataUser[0]}': " ".join(dataUser[1:])}
@@ -57,91 +68,67 @@ def addData(dataUser):
 
         with open(fileData, 'w') as file:
             json.dump(base, file)
+        print(cd('The data block was successfully added.', 'green'))
         start()
     else:
-        print("The data for the block is empty!")
+        print(cd("The data for the block is empty!", 'red'))
         start()
 
-
-def delData():
+def delBlock(dataUser):
     with open(fileData, 'r') as file:
         data = json.load(file)
 
     if data['datas'] == {}:
-        ('————————— Error —————————————————————')
-        print('The data does not exist!')
-        ('—————————————————————————————————————')
-        time.sleep(1)
+        print(cd('The data does not exist!', 'red'))
         start()
     else:
-
         order = 1
-        titleData = data['datas'].keys()
+        listBlocks = data['datas'].keys()
 
-        print(f'———————————————— Titles {20*'—'}')
-        for key in titleData:
+        print(f'{20*"—"} Titles {20*"—"}')
+        for key in listBlocks:
             print(f'{order}.', key)
             order += 1
-        print(f'{45*'—'}')
+        print(f'{48*"—"}')
 
-        userChoice = input('Delete all passwords or a specific one? (All/One/x): ').lower()
-
-        if userChoice == 'all':
-            print(f'———————————————— Code word {20*'—'}')
-            codeWord = input('Enter the code word: ')
-            print(f'{47*'—'}')
-            if data['code_word'] == codeWord:
-                data['datas'] = {}
-                with open(fileData, 'w') as file:
-                    json.dump(data, file)
-                start()
+        selectBlock = input('Write the name or number to view: ').strip()
+        order = 0
+        delBlock = None
+        try:
+            for key in listBlocks:
+                if int(selectBlock)-1 == order:
+                    delBlock = key
+                    break
+                order += 1
+            if delBlock:
+                print(f'{48*"—"}')
+                print(cd(f"{delBlock}: {data['datas'][delBlock]}", 'red'))
+                print(f'{48*"—"}')
+                choice = input(f"Delete a block of data? ({cd('Y', 'green')}/{cd('n', 'red')}): ")
+                if choice == 'Y':
+                    data['datas'].pop(key)
+                    with open(fileData, 'w') as file:
+                        json.dump(data, file)
             else:
-                clearDisplay()
-                ('————————— Error —————————————————————')
-                print('The code word is incorrect!')
-                ('—————————————————————————————————————')
-                time.sleep(1)
+                print(cd('Error!', 'red'))
+            start() 
+        except ValueError:
+            try:
+                if selectBlock in data['datas']:
+                    print(f'{48*"—"}')
+                    print(cd(f'{selectBlock}: {data["datas"][selectBlock]}', 'red'))
+                    print(f'{48*"—"}')
+                    choice = input(f"Delete a block of data? ({cd('Y', 'green')}/{cd('n', 'red')}): ")
+                    if choice == 'Y':
+                        data['datas'].pop(selectBlock)
+                        with open(fileData, 'w') as file:
+                            json.dump(data, file)
+                else:
+                    print(cd('Error!', 'red'))
                 start()
-        elif userChoice == 'x':
-            start()
-
-        clearDisplay()
-        order = 1
-        titleData = data['datas'].keys()
-        print(f'———————————————— Titles {20*'—'}')
-        for key in titleData:
-            print(f'{order}.', key)
-            order += 1
-        print(f'{44*'—'}')
-
-        nameOfTitle = input('Select the name of the data to delete (x): ')
-
-        if nameOfTitle in data['datas']:
-            print(f'————————————————— {nameOfTitle.strip()}{20*'—'}')
-            print(' '.join(data['datas'][nameOfTitle]))
-            print(f'{(38+len(nameOfTitle.strip()))*'—'}')
-            if input('Delete? (Y): ') == 'Y':
-                data['datas'].pop(nameOfTitle)
-                with open(fileData, 'w') as file:
-                    json.dump(data, file)
+            except KeyError:        
                 start()
-            else:
-                clearDisplay()
-                ('————————— Error —————————————————————')
-                print('Deletion error.')
-                ('—————————————————————————————————————')
-                time.sleep(1)
-                start()
-        elif nameOfTitle == 'x':
-            start()
-        else:
-            clearDisplay()
-            ('————————— Error —————————————————————')
-            print('There is no such name')
-            ('—————————————————————————————————————')
-            time.sleep(1)
-            start()
-
+'''
 def editData():
     with open(fileData, 'r') as file:
         data = json.load(file)
@@ -183,23 +170,41 @@ def editData():
             with open(fileData, 'w') as file:
                     json.dump(data, file)
             start()
-
+'''
 selection = {
         'show': showData,
-        'add': addData,
-        'del': delData,
-        'edit': editData
+        'add': addBlock,
+        'delete': delBlock,
+        #'edit': editBlock
     }
 
 def start():
-    while True:
-        userChoice = input("Commands' list (l): ").strip().split() 
-        try:
-            if userChoice[0].lower() == 'exit':
+    with open(fileData, 'r') as file:
+        base = json.load(file)
+    for i in range(5):
+        if base['code_word'] == '':
+            print(cd("Code words are needed to log into your database!", 'red'))
+            code_word = input(cd("Come up with 5 code words: ", 'green')).strip()
+            if len(code_word.split()) == 5:
+                base['code_word'] = code_word
+                with open(fileData, 'w') as file:
+                    json.dump(base, file)
                 break
-            selection[userChoice[0].lower()](userChoice[1:])
-        except (KeyError, IndexError):
-            print(help.helper)
+            else:
+                print(cd("The number of code words is not equal to 5!", 'red'))
+        else:
+            break
+    if base['code_word'] == '':
+        print("ERROR! Try again.")
+    else:
+        while True:
+            userChoice = input(cd("Commands' list (l): ", 'yellow')).strip().split() 
+            try:
+                if userChoice[0].lower() == 'exit':
+                    break
+                selection[userChoice[0].lower()](userChoice[1:])
+            except (KeyError, IndexError):
+                print(help.helper)
             
-
-start()
+if __name__ == '__main__':
+    start()
