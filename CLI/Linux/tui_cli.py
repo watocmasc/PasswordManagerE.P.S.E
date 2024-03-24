9810764f2167589d3ec67a1327289842beb70422
Watoc Masc <oushen.m4@gmail.com>
2024-03-25 01:02:51 +0200

    create tui-window for sig in

--- CLI/Linux/tui_cli.py
+++ CLI/Linux/tui_cli.py
@@ -1,26 +1,33 @@
 from pytermgui import widgets, WindowManager
-import pytermgui as ptg
+import pytermgui as ptg, time
 
-label = ptg.Label("Now get")
-input_Field = ptg.InputField("Code words")
-def on_click():
-    global label
-    label.value = input_Field.value
+code_words = "python" #test code words
 
-button = ptg.Button("Click", on_click())
-
-
-def on_click():
-    label = ptg.Label(input_Field)
+def on_click(inx):
+    if input_Field.value == code_words:
+        label = ptg.Label("Access")
+        window._add_widget(label)
+    else:
+        label = ptg.Label("Incorrect code words!")
+        window.__add__(label)
+        time.sleep(1)
+        window.remove(label)
+        window.height = 6
 
 with ptg.WindowManager() as manager:
+
+    label = ""
+    step_local = ""
+    input_Field = ptg.InputField(prompt="Code words: ")
+    button = ptg.Button("Login", onclick=on_click)
     window = ptg.Window(
-        "",
         input_Field,
-        "",
-        label,
         "",
         button,
         ""
     )
+    window.center(0)
+    window.height = 6
+    window.min_width = 50
+    window.set_title("Sign in")
     manager.add(window)
