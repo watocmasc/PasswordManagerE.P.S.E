e4bb03335ab176079e9e291aef8d599b2f40315c
Watoc <oushen.m4@gmail.com>
2024-03-24 20:14:17 +0200

    update test version tui

--- CLI/Linux/tui_cli.py
+++ CLI/Linux/tui_cli.py
@@ -1,54 +1,74 @@
-from subprocess import run
-
-from argparse import ArgumentParser, Namespace
 import pytermgui as ptg
+from pytermgui.pretty import pprint
 
-class MyTextInput(ptg.InputField):
-    def __init__(self, x, y, on_submit=None):
-        super().__init__(x, y, on_submit=on_submit)
-
-class TUI(ptg.Window):
-    def __init__(self, title, width, height):
-        super().__init__(self, title, width, height)
-        self.text_input = MyTextInput(10, 10)
-        self._add_widget(self.text_input)
-
-if __name__ == "__main__":
-   window = TUI('Hello', 50, 50)
-   window.run()
-
-'''
-
-def main() -> None:
-    """Main method"""
-   
-    with ptg.WindowManager() as manager:
-        loader = ptg.YamlLoader()
-        #namespace = loader.load(CONFIG_YAML)
-        def on_tap(window):
-            nowuser = ptg.InputField.value
-            print(nowuser)
-        window = (
-            ptg.Window(width=70)
-            + "[210 bold]Kitty Remote"
-            + "[210 bold]------------"
-            + ""
-            + ptg.Label(
-                "[240 italic]> Press RETURN to run each command", parent_align=0
-            )
-            + ptg.InputField("Code words: ")
-            + ""
-            + ""
-        ).center()
-        window._add_widget(ptg.Button("Sign in", on_tap(window)))
-
-        window.box = "DOUBLE"
-        
-        manager.add(window)
-
-        manager.run()
+OUTPUT = {}
 
 
-if __name__ == "__main__":
-    main()
-'''
+def submit(manager: ptg.WindowManager, window: ptg.Window) -> None:
+    for widget in window:
+        if isinstance(widget, ptg.InputField):
+            OUTPUT[widget.prompt] = widget.value
+            continue
+
+        if isinstance(widget, ptg.Container):
+            label, field = iter(widget)
+            OUTPUT[label.value] = field.value
+
+    manager.stop()
+
+
+CONFIG = """
+config:
+    InputField:
+        styles:
+            prompt: dim italic
+            cursor: '@72'
+    Label:
+        styles:
+            value: dim bold
+
+    Window:
+        styles:
+            border: '60'
+            corner: '60'
+
+    Container:
+        styles:
+            border: '96'
+            corner: '96'
+"""
+
+with ptg.YamlLoader() as loader:
+    loader.load(CONFIG)
+
+
+with ptg.WindowManager() as manager:
+    window = (
+        ptg.Window(
+            "",
+            ptg.InputField("Balazs", prompt="Name: "),
+            ptg.InputField("Some street", prompt="Address: "),
+            ptg.InputField("+11 0 123 456", prompt="Phone number: "),
+            "",
+            ptg.Container(
+                "Additional notes:",
+                ptg.InputField(
+                    "A whole bunch of\nMeaningful notes\nand stuff", multiline=True
+                ),
+                box="EMPTY_VERTICAL",
+            ),
+            "",
+            ["Submit", lambda *_: submit(manager, window)],
+            width=60,
+            box="DOUBLE",
+        )
+        .set_title("[210 bold]New contact")
+        .center()
+    )
+
+    # For the screenshot's sake
+    window.select(0)
+
+    manager.add(window)
+
+# pprint(OUTPUT)
